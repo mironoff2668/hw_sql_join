4bd4adf88817ea867593f6965b87240f31f9dc23
miron_iwmqnvh <mironoff2668@gmail.com>
2023-03-16 20:15:22 +0300

    change select_2

--- select_2.sql
+++ select_2.sql
@@ -18,16 +18,21 @@ order by album_id;
 
 --Все исполнители, которые не выпустили альбомы в 2020 году.
 select am.name from artist_music am
-join album_artist aa on am.artist_id = aa.artist_id 
-join album_music am2 on aa.album_id = am2.album_id
-where release_year != '2020';
-
+where am.name not in (
+		select am2.name from artist_music am2
+		join album_artist aa on am2.artist_id = aa.artist_id
+		join album_music am3 on aa.album_id = am3.album_id 
+		where release_year = '2020'
+		);
 
 --Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами).
-select cm.name from collection_music cm 
-join album_music am on cm.album_id = am.album_id
-join artist_music am2 on am.album_id = am2.artist_id
-where am2.name = 'Pet Shop Boys';
+select distinct cm.name from collection_music cm
+join track_music tm on cm.track_id = tm.track_id 
+join album_music am on tm.album_id = am.album_id
+join album_artist aa on am.album_id = aa.album_id
+join artist_music am2 on aa.artist_id =am2.artist_id 
+where am2.name = 'Pet Shop Boys'
+order by cm.name;
 
 --Названия альбомов, в которых присутствуют исполнители более чем одного жанра.
 select am.name, COUNT(gm.name)  from album_music am
@@ -47,21 +52,23 @@ where cm.track_id is null;
 --Исполнитель или исполнители, написавшие самый короткий по продолжительности трек,
 -- — теоретически таких треков может быть несколько.
 select am2.name, tm.duration from track_music tm
-join collection_music cm on tm.track_id = cm.track_id
-join album_music am3 on cm.album_id = am3.album_id 
-join artist_music am2 on am3.album_id = am2.artist_id
-group by am2.name, tm.duration
+join album_music am on tm.album_id = am.album_id 
+join album_artist aa on am.album_id = aa.album_id 
+join artist_music am2 on aa.artist_id = am2.artist_id 
+group by am2.name, tm.duration 
 having tm.duration = (select min(duration) from track_music)
 order by am2.name;
 
-
 --Названия альбомов, содержащих наименьшее количество треков.
-select am.name, COUNT(tm.name) from album_music am
-join track_music tm on am.album_id = tm.album_id 
-group by am.album_id 
-having COUNT(tm.name) = (
-		select COUNT(tm.name) from album_music am2
-		join track_music tm2 on am2.album_id = tm2.album_id
-		group by am2.album_id
-		order by COUNT(tm2.name)
-		limit 1);
+select distinct am.name from album_music am 
+left join track_music tm on tm.album_id = am.album_id
+where tm.album_id in (
+	select album_id from track_music
+	group by album_id
+	having count(track_id) = (
+		select count(track_id) from track_music
+		group by album_id
+		order by count
+		limit 1)
+)
+order by am.name;
